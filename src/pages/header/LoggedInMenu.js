419d16827bd8a633a63025eda4af0df02f24b7b3
Quidiox <33523865+Quidiox@users.noreply.github.com>
2018-07-26 13:19:20 +0300

    md-icons for menuItems and styling

--- src/pages/header/LoggedInMenu.js
+++ src/pages/header/LoggedInMenu.js
@@ -4,6 +4,10 @@ import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
 import AccountCircle from '@material-ui/icons/AccountCircle'
 import { NavLink } from 'react-router-dom'
+import AccountEditIcon from 'mdi-react/AccountEditIcon'
+import AccountRemoveIcon from 'mdi-react/AccountRemoveIcon'
+import AccountOffIcon from 'mdi-react/AccountOffIcon'
+import CloseIcon from 'mdi-react/CloseIcon'
 
 const LoggedInMenu = ({
   personAnchorEl,
@@ -39,14 +43,36 @@ const LoggedInMenu = ({
       open={Boolean(personAnchorEl)}
       onClose={handleClose}
     >
-      <MenuItem onClick={handleClose}>Close</MenuItem>
-      <MenuItem onClick={handleLogout} className={classes.logout}>
-        <NavLink to="/">Logout</NavLink>
+      <MenuItem onClick={handleClose} className={menuItemStyle}>
+        <CloseIcon style={iconStyle} />Close
       </MenuItem>
-      <MenuItem onClick={handleEdit}><NavLink to='/user/edit'>Edit account</NavLink></MenuItem>
-      <MenuItem onClick={handleDelete}>Delete account</MenuItem>
+      <MenuItem onClick={handleLogout} className={menuItemStyle}>
+        <AccountOffIcon style={iconStyle} />
+        <NavLink to="/">
+          <span style={menuItemStyle}>Logout</span>
+        </NavLink>
+      </MenuItem>
+      <MenuItem onClick={handleEdit} className={menuItemStyle}>
+        <AccountEditIcon style={iconStyle} />
+        <NavLink to="/user/edit">
+          <span style={menuItemStyle}>Edit account</span>
+        </NavLink>
+      </MenuItem>
+      <MenuItem onClick={handleDelete} className={menuItemStyle}>
+        <AccountRemoveIcon style={iconStyle} />Delete account
+      </MenuItem>
     </Menu>
   </div>
 )
+
+const menuItemStyle = {
+  color: 'black'
+}
+
+const iconStyle = {
+  height: '18px',
+  width: '18px',
+  marginRight: '5px'
+}
 
 export default LoggedInMenu
